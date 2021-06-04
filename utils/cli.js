5e1727b45aaf384ab08038eeb16ea0d89d858aee
msaaddev <mrsaadirfan@gmail.com>
2021-06-05 00:47:25 +0500

    👌 IMPROVE: experience

--- utils/cli.js
+++ utils/cli.js
@@ -3,8 +3,12 @@ const execa = require('execa');
 const chalk = require('chalk');
 const pwa = require('./pwa');
 
-module.exports = async name => {
+module.exports = async (name, flag) => {
 	const spinner = ora();
+
+	if (flag.length === 0) {
+		console.log('');
+	}
 	spinner.start(`${chalk.bold.dim('Creating Next.js App...')}`);
 
 	try {
@@ -12,10 +16,11 @@ module.exports = async name => {
 		await execa(`npx`, [`create-next-app`, `${name}`]);
 
 		// succeed
-		spinner.succeed(`${chalk.green('Next.js App Created.')}`);
+		spinner.succeed(`${chalk.green('Next.js App Created')}`);
 
 		await pwa(name);
 	} catch (err) {
+		console.log(err);
 		spinner.fail(`Couldn't create an app.`);
 	}
 };
--- utils/pwa.js
+++ utils/pwa.js
@@ -3,6 +3,7 @@ const execa = require('execa');
 const cwd = process.cwd();
 const jsonFile = require('jsonfile');
 const chalk = require('chalk');
+const logSymbols = require('log-symbols');
 const manifest = require('../pwa/pwa-manifest.json');
 const pwaPrettier = require('../pwa/prettier.json');
 const packageJSON = require('../pwa/pwa-package.json');
@@ -11,36 +12,58 @@ module.exports = async name => {
 	const spinner = ora();
 	const path = `${cwd}/${name}`;
 
-	// creating prettier configuration
-	execa('touch', [`${path}/.prettierrc.json`]);
+	try {
+		// creating prettier configuration
+		spinner.start(`${chalk.bold.dim('Creating PWA configurations...')}`);
+		execa('touch', [`${path}/.prettierrc.json`]);
 
-	// creating manifest.json file
-	execa('touch', [`${path}/public/manifest.json`]);
+		// copying logos
+		execa('cp', [`./img/logo-128x128.png`, `${path}/public`]);
+		execa('cp', [`./img/logo-512x512.png`, `${path}/public`]);
+		execa('cp', [`./pwa/_document.js`, `${path}/pages`]);
+		execa('cp', [`./pwa/next.config.js`, `${path}`]);
 
-	// copying logos
-	execa('cp', [`./img/logo-128x128.png`, `${path}/public`]);
-	execa('cp', [`./img/logo-512x512.png`, `${path}/public`]);
-	execa('cp', [`./pwa/_document.js`, `${path}/pages`]);
-	execa('cp', [`./pwa/next.config.js`, `${path}`]);
+		spinner.succeed(`${chalk.green('PWA configurations created.')}`);
 
-	// adding content to manifest.json
-	const pwaManifest = { ...manifest };
-	pwaManifest.name = name;
-	pwaManifest.short_name = name;
+		// creating manifest.json file
+		spinner.start(`${chalk.bold.dim('Creating manifest.json...')}`);
+		execa('touch', [`${path}/public/manifest.json`]);
 
-	// adding content to package.json
-	const pwaPkgJSON = { ...packageJSON };
-	pwaPkgJSON.name = name;
+		// adding content to manifest.json
+		const pwaManifest = { ...manifest };
+		pwaManifest.name = name;
+		pwaManifest.short_name = name;
+		spinner.succeed(`${chalk.green('manifest.json created.')}`);
 
-	// writing data to files
-	jsonFile.writeFile(`${path}/public/manifest.json`, pwaManifest, err => {});
-	jsonFile.writeFile(`${path}/.prettierrc.json`, pwaPrettier, err => {});
-	jsonFile.writeFile(`${path}/package.json`, pwaPkgJSON, err => {});
+		// adding content to package.json
+		spinner.start(`${chalk.bold.dim('Updating metadata files...')}`);
+		const pwaPkgJSON = { ...packageJSON };
+		pwaPkgJSON.name = name;
 
-	// installing packages
-	spinner.start(`${chalk.bold.dim('Installing dependencies...')}`);
-	await execa(`npm`, [`--prefix`, `${path}`, `install`]);
-	await execa(`npm`, [`--prefix`, `${path}`, `install`, `--only=dev`]);
-	await execa(`npm`, [`--prefix`, `${path}`, `run`, `format`]);
-	spinner.succeed(`${chalk.green('Dependencies installed.')}`);
+		// writing data to files
+		jsonFile.writeFile(
+			`${path}/public/manifest.json`,
+			pwaManifest,
+			err => {}
+		);
+		jsonFile.writeFile(`${path}/.prettierrc.json`, pwaPrettier, err => {});
+		jsonFile.writeFile(`${path}/package.json`, pwaPkgJSON, err => {});
+		spinner.succeed(`${chalk.green('metadata files updated.')}`);
+
+		// installing packages
+		spinner.start(`${chalk.bold.dim('Installing dependencies...')}`);
+		await execa(`npm`, [`--prefix`, `${path}`, `install`]);
+		await execa(`npm`, [`--prefix`, `${path}`, `install`, `--only=dev`]);
+		await execa(`npm`, [`--prefix`, `${path}`, `run`, `format`]);
+		spinner.succeed(`${chalk.green('Dependencies installed.')}`);
+
+		console.log('');
+		console.log(
+			logSymbols.success,
+			chalk.bgGreen.bold(` Next.js PWA `).black,
+			'created successfully.'
+		);
+	} catch (error) {
+		spinner.fail(`Couldn't convert Next.js app to PWA.`);
+	}
 };
