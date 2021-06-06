a2cbfac59baadc9686416a73e5a573d4706cef8f
msaaddev <mrsaadirfan@gmail.com>
2021-06-07 02:10:28 +0500

    👌 IMPROVE: err log

--- utils/pwa.js
+++ utils/pwa.js
@@ -98,11 +98,9 @@ module.exports = async (name, currentDir, isTailwind = false) => {
 
 		// writing data to files
 		if (!isWindows) {
-			jsonFile.writeFile(
-				`${pwaPaths.manifestFile}`,
-				pwaManifest,
-				err => {}
-			);
+			jsonFile.writeFile(`${pwaPaths.manifestFile}`, pwaManifest, err => {
+				err ? console.log(err) : null;
+			});
 			jsonFile.writeFile(
 				`${pwaPaths.prettierFile}`,
 				pwaPrettier,
