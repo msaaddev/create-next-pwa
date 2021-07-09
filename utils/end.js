b3f2a0a8ec70e8f41ccff0639800e6192d7f259d
msaaddev <mrsaadirfan@gmail.com>
2021-07-10 02:49:38 +0500

    📦 NEW: msg

--- utils/end.js
+++ utils/end.js
@@ -71,4 +71,12 @@ module.exports = async (name, isTailwind = false) => {
 	console.log(table.toString());
 
 	await notifyUpdate();
+
+	console.log();
+	console.log(
+		`${logSymbols.info} ${chalk.dim(
+			'Liked my work?! Nominate @msaaddev for GitHub star: https://stars.github.com'
+		)}`
+	);
+	console.log();
 };
